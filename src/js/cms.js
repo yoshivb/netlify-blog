ddc470f1dd730c8e9af1d761b56128a93f9f9ed8
yoshivb <yoshivanbelkom@msn.com>
2023-03-04 17:53:13 +0100

    Give up on home & games CMS pages

--- src/js/cms.js
+++ src/js/cms.js
@@ -4,14 +4,10 @@ import CMS from "netlify-cms-app";
 // Import main site styles as a string to inject into the CMS preview pane
 import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../css/main.scss";
 
-import HomePreview from "./cms-preview-templates/home";
 import PostPreview from "./cms-preview-templates/post";
 import ProjectPreview from "./cms-preview-templates/project";
-//import GamesPreview from "./cms-preview-templates/games"; //TODO
 
 CMS.registerPreviewStyle(styles, { raw: true });
-CMS.registerPreviewTemplate("home", HomePreview);
 CMS.registerPreviewTemplate("post", PostPreview);
 CMS.registerPreviewTemplate("project", ProjectPreview);
-//CMS.registerPreviewTemplate("games", GamesPreview); //TODO
 CMS.init();
